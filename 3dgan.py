cccc95116935afe38dbac80a50856544b45bd4a9
songyih <songyih1996@gmail.com>
2019-04-06 13:14:24 -0700

    mnist test

--- 3dgan.py
+++ 3dgan.py
@@ -15,7 +15,7 @@ parser.add_argument( '--n_epochs',
                      help='number of epochs of training' )
 parser.add_argument( '--batch_size',
                      type=int,
-                     default=128,
+                     default=64,
                      help='size of the batches' )
 parser.add_argument( '--lr_G',
                      type=float,
